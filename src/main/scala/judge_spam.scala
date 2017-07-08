6e8467db3bd91b7b9b6d2418aef7a068a8613e6b
masaki_kuwako <m.kuwako0702@gmail.com>
2017-07-08 23:52:39 +0900

    スパムの分類実装開始

---
+++ src/main/scala/judge_spam.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by m_kuwako on 2017/07/08.
+  */
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.classification.LogisticRegressionWithSGD
+
+class judge_spam {
+
+}
