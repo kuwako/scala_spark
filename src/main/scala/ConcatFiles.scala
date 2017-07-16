2ade3c26510087596ba4defa89f23c389871687d
masaki_kuwako <m.kuwako0702@gmail.com>
2017-07-16 23:25:13 +0900

    テストデータ用意

---
+++ src/main/scala/ConcatFiles.scala
@@ -0,0 +1,30 @@
+/**
+  * Created by m_kuwako on 2017/07/16.
+  */
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object ConcatFiles {
+  def main(args: Array[String]) :Unit = {
+    val conf = new SparkConf().setAppName("ConcatFiles").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    // TODO 全ファイルの読み出し
+    val inputRDD = sc.textFile("src/resources/livedoor/*/*.txt")
+
+    // TODO 連結
+    val buffer = inputRDD.flatMap(line => line.split(" "))
+    printRDD("flatmap", buffer)
+
+    // TODO 保存
+  }
+
+  def printRDD(filterName: String, rdd: org.apache.spark.rdd.RDD[_]) = {
+    println(filterName)
+
+    rdd.foreach {r => {
+      println(r)
+    }
+    }
+  }
+}
