b52eb62b0e54912a2e9b3c35a37850cb5afd427b
masaki_kuwako <m.kuwako0702@gmail.com>
2017-07-30 03:32:12 +0900

    word2vecの実装開始

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
 import org.apache.spark.ml.feature.HashingTF
 import org.apache.spark.{SparkConf, SparkContext}
+import scala.collection.convert.WrapAsScala._
+import org.apache.spark.mllib.feature.Word2Vec
+import com.atilika.kuromoji.ipadic.{Token,Tokenizer}
 
 /**
   * Created by m_kuwako on 2017/07/29.
@@ -10,6 +13,23 @@ object Main {
     val sc = new SparkContext(conf)
 
     val inputRdd = sc.textFile("data/output.txt")
-    val tf = new HashingTF(numFeatures = 100)
+
+    val input = inputRdd.map(line => {
+      val builder = new Tokenizer.Builder()
+      val tokenizer = builder.build()
+      val tokens = tokenizer.tokenize(line)
+      val output : StringBuilder = new StringBuilder()
+      tokens.foreach(token => {
+        if(token.getAllFeatures().indexOf("名詞") != -1) {
+          output.append(token.getSurface)
+          output.append(" ")
+        }
+      })
+      output.toString() // return
+    }).map(line => line.split(" ").toSeq)
+
+    val Word2Vec = new Word2Vec()
+    val model = Word2Vec.fit(input)
+    for((synonym, cosineSimilarity) <- model.findSynonyms("mac", 40)) { println(s"$synonym $cosineSimilarity") }
   }
 }
