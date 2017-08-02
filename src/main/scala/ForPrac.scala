cc5a40e558f0d4c5b5a1afaad2a1ef137d3575ac
kuwako <m.kuwako0702@gmail.com>
2017-08-02 18:54:05 +0900

    サンプルの動作検証

---
+++ src/main/scala/ForPrac.scala
@@ -0,0 +1,14 @@
+object ForPrac {
+    def main(args: Array[String]) :Unit = {
+        var buff = ""
+        val sample = "sample"
+
+        sample.foreach(node => {
+            println(node)
+            buff = buff + node
+        })
+
+        println("buff is ")
+        println(buff)
+    }
+}
