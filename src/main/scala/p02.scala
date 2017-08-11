7b378217d38d419b08e498c3e7063c0dfb4aed1d
masaki_kuwako <m.kuwako0702@gmail.com>
2017-08-12 00:19:08 +0900

    p02完了

--- src/main/scala/p02.scala
+++ src/main/scala/p02.scala
@@ -3,11 +3,12 @@ object p02 {
   def main(args: Array[String]): Unit = {
     val list = List(1, 3, 5, 7 , 9)
 
-    println(beforeLast(list))
+    println(penultimate(list))
   }
 
-  def beforeLast[T](list: List[T]): T = {
-    list.foreach(node => {
-    })
+  def penultimate[T](list: List[T]): T = list match {
+    case x :: _ :: Nil => x
+    case _ :: y :: xs => penultimate(y :: xs)
+    case _ => sys.error("something wrong")
   }
 }
