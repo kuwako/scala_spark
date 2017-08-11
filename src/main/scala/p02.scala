746d1a397e6ee88063834918cce99316ce02a4ff
masaki_kuwako <m.kuwako0702@gmail.com>
2017-08-11 23:57:32 +0900

    p02追加

---
+++ src/main/scala/p02.scala
@@ -0,0 +1,13 @@
+// Problem 2 – Find the last but one element of a list
+object p02 {
+  def main(args: Array[String]): Unit = {
+    val list = List(1, 3, 5, 7 , 9)
+
+    println(beforeLast(list))
+  }
+
+  def beforeLast[T](list: List[T]): T = {
+    list.foreach(node => {
+    })
+  }
+}
