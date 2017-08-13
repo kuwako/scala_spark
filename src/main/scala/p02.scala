d0f6a41c1d30c4f8d9a9f4c6546f26b08941efce
masaki_kuwako <m.kuwako0702@gmail.com>
2017-08-13 23:59:01 +0900

    p03

--- src/main/scala/p02.scala
+++ src/main/scala/p02.scala
@@ -1,4 +1,6 @@
 // Problem 2 – Find the last but one element of a list
+// http://aperiodic.net/phil/scala/s-99/
+// https://github.com/dwango/S99/tree/master/src/main/scala/jp/co/dwango/s99
 object p02 {
   def main(args: Array[String]): Unit = {
     val list = List(1, 3, 5, 7 , 9)
