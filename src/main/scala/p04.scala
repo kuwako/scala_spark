0563ab57e3bfa62b52e2f9a63edd0ba6c92867e3
masaki_kuwako <m.kuwako0702@gmail.com>
2017-08-15 23:18:13 +0900

    p04

---
+++ src/main/scala/p04.scala
@@ -0,0 +1,19 @@
+import scala.annotation.tailrec
+
+// Problem 4 Find the number of elements of a list.
+// http://aperiodic.net/phil/scala/s-99/
+// https://github.com/dwango/S99/tree/master/src/main/scala/jp/co/dwango/s99
+object p04 {
+  def main(args: Array[String]): Unit = {
+    val list = List(1, 3, 5, 7 , 9, 11, 13)
+
+    println(length(list))
+  }
+
+  @tailrec
+  def length[T](list: List[T], i: Int = 1): Int = list match {
+    case x :: Nil => i
+    case _ :: xs => length(xs, i + 1)
+    case _ => sys.error("something wrong")
+  }
+}
