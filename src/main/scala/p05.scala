28d1926f9f9bb1641815c1631b5a2bdc845d9551
masaki_kuwako <m.kuwako0702@gmail.com>
2017-08-16 00:51:19 +0900

    p05

--- src/main/scala/p05.scala
+++ src/main/scala/p05.scala
@@ -1,19 +1,19 @@
 import scala.annotation.tailrec
 
-// Problem 4 Find the number of elements of a list.
+// Problem 5  Reverse a list.
 // http://aperiodic.net/phil/scala/s-99/
 // https://github.com/dwango/S99/tree/master/src/main/scala/jp/co/dwango/s99
-object p04 {
+object p05 {
   def main(args: Array[String]): Unit = {
     val list = List(1, 3, 5, 7 , 9, 11, 13)
 
-    println(length(list))
+    println(reverse(list))
   }
 
   @tailrec
-  def length[T](list: List[T], i: Int = 1): Int = list match {
-    case x :: Nil => i
-    case _ :: xs => length(xs, i + 1)
+  def reverse[T](list: List[T], buffer: List[T] = Nil): List[T] = list match {
+    case Nil => buffer
+    case x :: xs => reverse(xs, x :: buffer)
     case _ => sys.error("something wrong")
   }
 }
