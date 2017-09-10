6308b881d191c9d5e28a2549039f7605ea7e4d01
masaki_kuwako <m.kuwako0702@gmail.com>
2017-09-10 23:54:02 +0900

    p27、とても難しい

--- src/main/scala/p27.scala
+++ src/main/scala/p27.scala
@@ -19,7 +19,7 @@
 object p27 {
   def main(args: Array[String]): Unit = {
     println(group3(List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")))
-//    println(group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")))
+    println(group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")))
   }
 
   def group3[T](list: List[T]): List[(List[T], List[T], List[T])] = {
@@ -30,7 +30,29 @@ object p27 {
       yield (groupWith2Elements, groupWith3Elements, list diff groupWith2Elements diff groupWith3Elements)
   }
 
-//  def group[T](order: List[Int], list: List[T]): List[List[List[T]]] = {
-//
-//  }
+  def group[T](setting: List[Int], list: List[T]): List[List[List[T]]] = {
+    def group_(setting: List[Int], n: Int, list: List[T]): List[List[List[T]]] = {
+      setting match {
+        case Nil => List(Nil)
+        case x::xs =>
+          groupInto(n, x, list).flatMap { case (s, r) => group_(xs, n - x, r).map(k => k.::(s)) }
+      }
+    }
+    group_(setting, list.size, list)
+  }
+
+  def groupInto[T](n: Int, k: Int, list: List[T]): List[(List[T], List[T])] = list match {
+    case _ if k == n => List((list, Nil))
+    case _ if k == 0 => List((Nil, list))
+    case x :: xs if k > 0 =>
+      append(
+        groupInto(n - 1, k - 1, xs).map{a => (x::a._1, a._2)},
+        groupInto(n - 1, k, xs).map{a => (a._1, x::a._2)})
+    case _ => error("")
+  }
+
+  def append[T](a: List[T], b: List[T]): List[T] = a match {
+    case Nil => b
+    case x::xs => x::append(xs, b)
+  }
 }
